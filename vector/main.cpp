da014aca20ba8627c78a347ab8a149fe28f6b247
BerlyCS <berly.mandamientos@gmail.com>
2024-06-12 23:40:18 -0500

    Cambios: Implementacion final vector, main y array.hpp

--- vector/main.cpp
+++ vector/main.cpp
@@ -2,36 +2,94 @@
 #include <ostream>
 #include <utility>
 #include "vector.hpp"
+#include "array.hpp"
 
 using namespace std;
 
-class test {
+class point {
+    private:
+        int x,y;
     public:
-        test() : a(1) {}
-        test(int a) : a(a) {}
-        int a;
+        point(int a, int b) : x(a), y(b) {}
+        point() : x(0), y(0) {}
+
+        friend ostream& operator<<(ostream& os, point val);
 };
 
+ostream& operator<<(ostream& os, point val) {
+    os<<'(' << val.x << ',' << val.y << ')' << ' ';
+    return os;
+}
+
 int main() {
-    vect<test> a;
-    a.push_back(test(4));
-    a.push_back(test(5));
-    a.push_back(test(4));
-    cout<<a.getsize()<<endl;
+    vect<point> a;
+    a.push_back(point(1,2));
+    a.push_back(point(3,4));
 
-    vect<test> b;
-    b.push_back(test(6));
-    b.push_back(test(4));
-    b.push_back(test(7));
-    cout<<b.getsize()<<endl;
+    vect<point> b(2, point(1,1));
 
-    vect<test> c = std::move(a);
-    cout<<c.getsize()<<endl;
-    cout<<endl<<a.getsize()<<endl;
+    a.print();
+    b.print();
 
-    vect<vect<int>> aa;
-    aa.push_back(vect<int>(2,0));
-    aa.push_back(vect<int>(3,3));
+    vect<point> c = a + b;
+    c.print();
 
-    cout<<aa;
+    c = std::move(b);
+    cout<<"b: "; b.print();
+    cout<<"c: ";c.print();
+
+    vect<point> d(std::move(a));
+    cout<<"a: "; a.print();
+    cout<<"d: "; d.print();
+
+    c = c*2;
+    cout<<"c*2: "; c.print();
+
+    point tmp = c.pop_back();
+    cout<<"tmp point: "<<tmp<<endl;
+    c += tmp;
+
+    cout<<"c size: "<<c.getsize()<<endl; 
+    cout<<"d size: "<<d.getsize()<<endl; 
+    (c<d) ? cout<<"true\n" : cout<<"false\n";
+    (c>d) ? cout<<"true\n" : cout<<"false\n";
+    (c==d) ? cout<<"true\n" : cout<<"false\n";
+    (c<=d) ? cout<<"true\n" : cout<<"false\n";
+    (c>=d) ? cout<<"true\n" : cout<<"false\n";
+
+    cout<<"-------Arrays--------\n";
+    Array<point, 3> x(point(2,2));
+    x.print();
+    
+    Array<point, 5> y;
+    y.set(0, point(1,2));
+    y.print();
+    
+    /* vect<test> a; */
+    /* a.push_back(test(4)); */
+    /* a.push_back(test(5)); */
+    /* a.push_back(test(4)); */
+    /* cout<<a.getsize()<<endl; */
+    /* cout<<a[0].a<<endl; */
+
+    /* vect<test> b; */
+    /* b.push_back(test(6)); */
+    /* b.push_back(test(4)); */
+    /* b.push_back(test(7)); */
+    /* cout<<b.getsize()<<endl; */
+    /* cout<<b[0].a<<endl; */
+
+    /* vect<test> c = std::move(a); */
+    /* cout<<c.getsize()<<' '<<a.getsize()<<endl; */
+
+    /* Array<test, 4> x; */
+    /* cout<<x[0].a<<endl; */
+
+    /* Array<test, 3> y; */
+    /* cout<<y[0].a<<endl; */
+
+    /* Array<test, 3> z = std::move(y); */
+    /* cout<<z[0].a<<' '<<endl; */
+        /* y[0].a<<endl; */ //Genera un error
+
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -25,19 +25,60 @@ template<class T> class vect {
         void insert(int index, T val);
         void remove(int index);
         void print();
-        vect<T> operator+(vect<T>);
-        vect<T> operator*(vect<T>);
-        vect<T> operator+=(vect<T>);
-        vect<T> operator-=(vect<T>);
-        vect<T> operator*=(int);
+        vect<T> operator+(vect<T>&);
+        vect<T> operator*(int);
+        void operator+=(vect<T>&);
+        void operator+=(T&);
+        void operator*=(int);
         bool operator<(vect<T>&);
         bool operator>(vect<T>&);
         bool operator==(vect<T>&);
         bool operator>=(vect<T>&);
         bool operator<=(vect<T>&);
-        vect<T> operator=(vect<T>&);
-        vect<T> operator=(vect<T>&&);
+        void operator=(vect<T>&);
+        void operator=(vect<T>&&);
 };
+
+template <class T>
+vect<T>::vect() {
+    size=0;
+    data = new T[size];
+}
+
+template <class T>
+vect<T>::vect(vect<T> &b) {
+    size = b.getsize();
+    data = new T[size];
+
+    for (int i=0; i<size; i++) {
+        data[i] = b.data[i];
+    }
+}
+
+template <class T>
+vect<T>::vect(int size) {
+    this->size = size;
+    data = new T[size];
+}
+template <class T>
+vect<T>::vect(int size, T start) {
+    this->size = size;
+    data = new T[size];
+    for (int i=0; i<size; i++) {
+        data[i] = start;
+    }
+}
+template <class T>
+vect<T>::~vect(){
+    delete[] data; 
+}
+
+template <class T>
+vect<T>::vect(vect<T>&& tmp) : data(tmp.data), size(tmp.size) {
+    tmp.data = nullptr;
+    tmp.size = 0;
+}
+
 template <class T>
 ostream& operator<<(ostream& os, vect<T>& obj) {
     for (int i =0; i<obj.getsize(); i++) {
@@ -47,7 +88,7 @@ ostream& operator<<(ostream& os, vect<T>& obj) {
 }
 
 template <class T>
-vect<T> vect<T>::operator=(vect<T>& tmp) {
+void vect<T>::operator=(vect<T>& tmp) {
     delete this->data;
     size = tmp.size;
     data = new T[size];
@@ -56,17 +97,59 @@ vect<T> vect<T>::operator=(vect<T>& tmp) {
     }
 }
 
+template <class T>
+void vect<T>::operator=(vect<T>&& tmp)
+{
+    delete this->data;
+    data = tmp.data, size = tmp.size;  
+    tmp.size = 0;
+    tmp.data = nullptr;
+}
 
 template<class T>
-vect<T> vect<T>::operator+(vect<T> b) {
+vect<T> vect<T>::operator+(vect<T>& b) {
     vect<T> c(b.size+size);
     for (int i = 0; i<size; i++) {
         c[i] = data[i];
     }
     for (int i = 0; i<b.size; i++) {
-        c[i+size-1] = b[i];
+        c[i+size] = b[i];
     }
     return c;
+}
+
+template<class T>
+void vect<T>::operator+=(vect<T>& b) {
+    for (int i = 0; i<b.size; i++) {
+        this->push_back(b[i]);
+    }
+}
+
+template<class T>
+void vect<T>::operator+=(T& elem) {
+    this->push_back(elem);
+}
+
+template <typename T>
+vect<T> vect<T>::operator*(int x) {
+    int nsize = size*x;
+    vect<T> res(nsize);
+    for (int i=0; i<nsize; i++) {
+        res[i] = data[i%size];
+    }
+    return res;
+}
+
+template <typename T>
+void vect<T>::operator*=(int x) {
+    int nsize = size*x;
+    T* tmp = new T[nsize];
+    for (int i=0; i<nsize; i++) {
+        tmp[i] = data[i%size];
+    }
+    delete data;
+    data= tmp;
+    size = nsize;
 }
 
 template<class T>
@@ -99,40 +182,6 @@ template <class T>
 T& vect<T>::operator[](int index) {
     return data[index];
 }
-
-template <class T>
-vect<T>::vect() {
-    size=0;
-    data = new T[size];
-}
-
-template <class T>
-vect<T>::vect(vect<T> &b) {
-    size = b.getsize();
-    data = new T[size];
-
-    for (int i=0; i<size; i++) {
-        data[i] = b.data[i];
-    }
-}
-
-template <class T>
-vect<T>::vect(int size) {
-    this->size = size;
-    data = new T[size];
-}
-
-template <class T>
-vect<T>::~vect(){
-    delete[] data; 
-}
-
-template <class T>
-vect<T>::vect(vect<T>&& tmp) : data(tmp.data), size(tmp.size) {
-    tmp.data = nullptr;
-    tmp.size = 0;
-}
-
 
 template <class T>
 void vect<T>::push_back(T val) {
@@ -189,9 +238,12 @@ void vect<T>::remove(int index) {
     size--;
 }
 
-
 template <class T>
 void vect<T>::print() {
+    if (size == 0) {
+        cout<<"Vacio.\n";
+        return;
+    }
     for (int i=0; i<size; i++) {
         cout<<data[i]<<" ";
     }
